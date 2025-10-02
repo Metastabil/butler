649f5b25fe3ebbac65ff570881c93a0f89e08d01
Julius <julius.derigs@mail.de>
2025-10-02 15:32:23 +0200

    Anpassen des BaseModels und Hinzufügen der Models.

---
+++ models/LogModel.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Models;
+
+/**
+ * @author Julius Derigs
+ * @version 1.0.0
+ */
+
+class LogModel extends BaseModel {
+    /**
+     * @var string
+     */
+    private string $folder = 'logs';
+
+    /**
+     * @param string $table
+     * @param int $record_id
+     * @return array
+     */
+    public function select(string $table, int $record_id) :array {
+        $query = $this->query($this->folder, 'select-by-table-and-record-id');
+        $params = [
+            'table_name' => $table,
+            'record_id' => $record_id
+        ];
+
+        $statement = $this->db->prepare($query);
+        $statement->execute($params);
+
+        return $statement->fetchAll();
+    }
+
+    /**
+     * @param array $data
+     * @return bool|int
+     */
+    public function insert(array $data) :bool|int {
+        $query = $this->query($this->folder, 'insert');
+
+        if ($this->db->prepare($query)->execute($data)) {
+            return $this->db->lastInsertId();
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function update(array $data) :bool {
+        $query = $this->query($this->folder, 'update');
+
+        return $this->db->prepare($query)->execute($data);
+    }
+}
---
+++ models/UserModel.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Models;
+
+/**
+ * @author Julius Derigs
+ * @version 1.0.0
+ */
+
+class UserModel extends BaseModel {
+    /**
+     * @var string
+     */
+    private string $folder = 'users';
+
+    /**
+     * @param int $id
+     * @param string $username
+     * @param bool $deleted
+     * @return array
+     */
+    public function select(int $id = 0, string $username = '', bool $deleted = false) :array {
+        $params['deleted'] = $deleted;
+
+        if ($id > 0) {
+            $query = $this->query($this->folder, 'select-by-id');
+            $params['id'] = $id;
+        }
+        elseif (!empty($username)) {
+            $query = $this->query($this->folder, 'select-by-username');
+            $params['username'] = $username;
+        }
+        else {
+            $query = $this->query($this->folder, 'select');
+        }
+
+        $statement = $this->db->prepare($query);
+        $statement->execute($params);
+
+        $result = $statement->fetchAll();
+
+        return !empty($result) && ($id > 0 || !empty($username)) ? $result[0] : $result;
+    }
+
+    /**
+     * @param array $data
+     * @return bool|int
+     */
+    public function insert(array $data) :bool|int {
+        $query = $this->query($this->folder, 'insert');
+
+        if ($this->db->prepare($query)->execute($data)) {
+            return $this->db->lastInsertId();
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function update(array $data) :bool {
+        $query = $this->query($this->folder, 'update');
+
+        return $this->db->prepare($query)->execute($data);
+    }
+}
