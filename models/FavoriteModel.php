58d7dea62cde85dd6800fdc770b918546297c00a
Julius <julius.derigs@mail.de>
2025-11-30 21:37:47 +0100

    Zwischestand Favoriten.

---
+++ models/FavoriteModel.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Models;
+
+/**
+ * @author Julius Derigs
+ * @version 1.0.0
+ */
+
+class FavoriteModel extends BaseModel {
+    /**
+     * @var string
+     */
+    private string $folder = 'favorites';
+
+    /**
+     * @param int $id
+     * @param bool $deleted
+     * @return array
+     */
+    public function select(int $id = 0, bool $deleted = false) :array {
+        // TODO
+    }
+
+    /**
+     * @param array $data
+     * @return bool|int
+     */
+    public function insert(array $data) :bool|int {
+        $query = $this->query($this->folder, 'insert');
+
+        if ($this->db->prepare($query)->execute($data)) {
+            return $this->db->lastInsertId();
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function update(array $data) :bool {
+        $query = $this->query($this->folder, 'update');
+
+        return $this->db->prepare($query)->execute($data);
+    }
+}
